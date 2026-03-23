745f03dc4ebf5a56d082cfeacf961fd7d1fcff92
thoompranay-maker <thoompranay@gmail.com>
2026-03-23 21:42:49 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,14 @@
-
+const familyData = {
+  name: "Grandfather",
+  image: "images/default.jpg",
+  address: "Village",
+  phone: "0000000000",
+  children: [
+    {
+      name: "Father",
+      image: "images/default.jpg",
+      address: "City",
+      phone: "1111111111"
+    }
+  ]
+};
